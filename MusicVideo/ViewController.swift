39d337c7e58b325aa69bbc4c1a925d11f49e2bbd
Wfuger <wfuger@gmail.com>
2016-05-28 21:36:04 -0600

    Reachability status changes are reflected in the view.

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -13,8 +13,14 @@ class ViewController: UIViewController {
     
     var videos = [Videos]()
     
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.reachabilityStatusChanged), name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
         
         //Call Api
         let api = APIManager()
@@ -28,15 +34,38 @@ class ViewController: UIViewController {
         
         self.videos = videos
         
-        for item in videos {
-            print("name = \(item.vName)")
-        }
+//        for item in videos {
+//            print("name = \(item.vName)")
+//        }
         
         for (index, item) in videos.enumerate() {
-            print("\(index) name = \(item.vArtist)")
+            print("\(index) artist(s) = \(item.vArtist)")
+            print("title = \(item.vName)")
+            print(" ")
+        }
+        
+        
+    }
+    func reachabilityStatusChanged() {
+        
+        switch reachabilityStatus {
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+        displayLabel.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+        displayLabel.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+        displayLabel.text = "Reachable with Cellular"
+        default:return
         }
         
     }
+    
+    
+    deinit
+    {
+    NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
+    
     
 }
 
