b1897008dd26d753333df9dcfb425afe986e65ae
Wfuger <wfuger@gmail.com>
2016-06-04 17:09:28 -0600

    Videos play now

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class MusicVideoDetailVC: UIViewController {
 
@@ -39,5 +41,20 @@ class MusicVideoDetailVC: UIViewController {
         
     }
 
+    @IBAction func playVideo(sender: UIBarButtonItem) {
+        
+        let url = NSURL(string: videos.vVideoUrl)!
+        
+        let player = AVPlayer(URL: url)
+        
+        let playerViewController = AVPlayerViewController()
+        
+        playerViewController.player = player
+        
+        self.presentViewController(playerViewController, animated: true) {
+            playerViewController.player?.play()
+        }
+        
+    }
     
 }
